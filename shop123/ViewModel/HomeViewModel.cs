5e2c6716edaee933b07012c11b31e6c79c17a76e
Torquecandy <leo755307@gmail.com>
2022-02-11 00:12:44 +0800

    加入購物車加入訂單、訂單明細 功能頁面初步完成
    很多細小功能要調整，先做出大概的樣子出來

--- shop123/ViewModel/HomeViewModel.cs
+++ shop123/ViewModel/HomeViewModel.cs
@@ -10,9 +10,7 @@ namespace shop123.ViewModel
     public class HomeViewModel
     {
         public IEnumerable<carousel> carousels { get; set; }
-        public IEnumerable<spu> spu { get; set; }
-        public IEnumerable<catalogB> categoryB { get; set; }
-        public IEnumerable<catalogA> categoryA { get; set; }
+        public IEnumerable<spu> spu { get; set; }  
         public IPagedList<shop123.Models.spu> spuList { get; set; }
 
 
