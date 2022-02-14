0f87e581fa4de4e78fb817582a363862064e71e6
TakahashiKaede <j36ef37@gmail.com>
2022-02-14 20:38:37 +0800

    上傳功能1.0

--- shop123/Controllers/SellerController.cs
+++ shop123/Controllers/SellerController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using shop123.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -33,6 +35,96 @@ namespace shop123.Controllers
                 );
             return View(products.ToList());
         }
+
+
+        public ActionResult upload()  //圖片上傳至Images資料夾，但與資料庫的名稱不同，待處理
+        {         
+            bool isSavedSuccessfully = true;
+            string fname = "";         
+            try
+            {
+                foreach (string filename in Request.Files)
+                {                  
+                    HttpPostedFileBase file = Request.Files[filename];
+                    fname = file.FileName;
+                    if (file != null && file.ContentLength > 0)
+                    {                       
+                        var path = Path.Combine(Server.MapPath("~/Images"));
+                        string pathstring = Path.Combine(path.ToString());
+                        string filename1 = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                        bool isexist = Directory.Exists(pathstring);
+                        if (!isexist)
+                        {
+                            Directory.CreateDirectory(pathstring);
+                        }
+                        string uploadpath = string.Format("{0}\\{1}", pathstring, filename1);
+                       
+                        file.SaveAs(uploadpath);                    
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                isSavedSuccessfully = false;
+
+            }
+            if (isSavedSuccessfully)
+            {
+
+                return new JsonResult()
+                {
+                    Data = new
+                    {
+                        Message = fname
+                    },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };              
+            }
+            else
+            {
+                return new JsonResult()
+                {
+                    Data = new
+                    {
+                        Message = "Error"
+                    },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+
+        }
+
+        public ActionResult SellerAddItem() //賣家商品資訊上傳
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SellerAddItem(SpuModel sm)
+        {
+            shop123Entities sh = new shop123Entities();
+            spu spu = new spu()
+            {
+                spuName = sm.spuName,
+                memberId = sm.memberId,
+                spuInfo = sm.spuInfo,
+                spuPrice = sm.spuPrice,
+                catalogAId = sm.catalogAId,
+                catalogBId = sm.catalogBId,
+                spuImg1 = sm.spuImg1,
+                spuImg2 = sm.spuImg2,
+                spuImg3 = sm.spuImg3,
+                spuImg4 = sm.spuImg4,
+                spuImg5 = sm.spuImg5,
+                spuShow = sm.spuShow,
+            };
+            spu.spuCreatedTime = DateTime.Now;
+            spu.spuEditTime = DateTime.Now;
+            sh.spu.Add(spu);
+            sh.SaveChanges();
+            return RedirectToAction("List");
+    
+        }
+
     }
 
 
---
+++ shop123/Models/SpuModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shop123.Models
+{
+    public class SpuModel
+    {
+        public string spuName { get; set; }
+        public int memberId { get; set; }
+        public string spuInfo { get; set; }
+        public int spuPrice { get; set; }
+        public int catalogAId { get; set; }
+        public int catalogBId { get; set; }
+        public string spuImg1 { get; set; }
+        public string spuImg2 { get; set; }
+        public string spuImg3 { get; set; }
+        public string spuImg4 { get; set; }
+        public string spuImg5 { get; set; }
+        public string spuShow { get; set; }
+        public string spuCreatedTime { get; set; }
+        public string spuEditTime { get; set; }
+    }
+}
