5550cadb4c347bbf9dc4aad3bd032e3231fc6824
Torquecandy <leo755307@gmail.com>
2022-02-16 22:21:01 +0800

    修正小錯誤

--- shop123/ViewModel/ShopCartViewModel.cs
+++ shop123/ViewModel/ShopCartViewModel.cs
@@ -7,26 +7,26 @@ namespace shop123.ViewModel
 {
     public class ShopCartViewModel
     {
-
-        var ODSKU = db.ordersDetail.Join(db.sku,
-                  k => k.skuId,
-                  u => u.id,
-                  (k, u) => new
-                  {
-                      id = k.id,
-                      memberId = k.memberId,
-                      orderguid = k.orderguid,
-                      skuId = k.skuId,
-                      spuImg1 = k.spuImg1,
-                      orderDetailspunamek = k.orderDetailspuname,
-                      orderDetailsize = k.orderDetailsize,
-                      orderDetailcolor = k.orderDetailcolor,
-                      orderDetailprice = k.orderDetailprice,
-                      orderDetailnum = k.orderDetailnum,
-                      orderDetailtotalprice = k.orderDetailtotalprice,
-                      orderDetailIsApproved = k.orderDetailIsApproved,
-                      @checked = k.@checked,
-                      spuid = u.spuId,
-                  }).Where(m => m.memberId == memberId && m.orderDetailIsApproved == "否").ToList();
+        //public 
+        //var ODSKU = db.ordersDetail.Join(db.sku,
+        //          k => k.skuId,
+        //          u => u.id,
+        //          (k, u) => new
+        //          {
+        //              id = k.id,
+        //              memberId = k.memberId,
+        //              orderguid = k.orderguid,
+        //              skuId = k.skuId,
+        //              spuImg1 = k.spuImg1,
+        //              orderDetailspunamek = k.orderDetailspuname,
+        //              orderDetailsize = k.orderDetailsize,
+        //              orderDetailcolor = k.orderDetailcolor,
+        //              orderDetailprice = k.orderDetailprice,
+        //              orderDetailnum = k.orderDetailnum,
+        //              orderDetailtotalprice = k.orderDetailtotalprice,
+        //              orderDetailIsApproved = k.orderDetailIsApproved,
+        //              @checked = k.@checked,
+        //              spuid = u.spuId,
+        //          }).Where(m => m.memberId == memberId && m.orderDetailIsApproved == "否").ToList();
     }
 }
