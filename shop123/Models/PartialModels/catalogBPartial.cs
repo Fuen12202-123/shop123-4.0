b391b2ecc4adbd8732099b00b185d39850b30f0c
依 <48285665+amanda5m@users.noreply.github.com>
2022-02-24 00:35:16 +0800

    呈現分類項目

---
+++ shop123/Models/PartialModels/catalogBPartial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace shop123.Models
+{
+    [MetadataType(typeof(catalogBMetadata))]
+    public partial class catalogB
+    {
+        public class catalogBMetadata
+        {
+            [DisplayName("分類編號")]
+            public int id { get; set; }
+            [DisplayName("分類細項")]
+            public string catalogBName { get; set; }
+            [DisplayName("分類大項")]
+            public int catalogAId { get; set; }
+        }
+
+    }
+    
+}
