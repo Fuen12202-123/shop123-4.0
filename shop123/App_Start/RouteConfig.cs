c1fc2cf1cb98504b02d300d0925b8e8d71e547f6
Torquecandy <leo755307@gmail.com>
2022-03-01 21:31:30 +0800

    修改route

--- shop123/App_Start/RouteConfig.cs
+++ shop123/App_Start/RouteConfig.cs
@@ -53,9 +53,60 @@ namespace shop123
            );
             routes.MapRoute(
                 name: "WOMAN",
-                url: "WOMAN/{catalogBId}/page/{page}/{sort}",
-                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = UrlParameter.Optional, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+                url: "WOMAN/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 0, page = UrlParameter.Optional , sort=UrlParameter.Optional }
             );
+               routes.MapRoute(
+                name: "tops/long",
+                url: "WOMAN/tops/long/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 1, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "tops/short",
+                url: "WOMAN/tops/short/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 2, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "top/svest",
+                url: "WOMAN/top/svest/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 3, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "shirts",
+                url: "WOMAN/shirts/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 4, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "dresses",
+                url: "WOMAN/dresses/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 5, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "outerwear",
+                url: "WOMAN/outerwear/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 6, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "bottoms/pants",
+                url: "WOMAN/bottoms/pants/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 7, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "bottoms/shorts",
+                url: "WOMAN/bottoms/shorts/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 8, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "bottoms/skirt",
+                url: "WOMAN/bottoms/skirt/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 9, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+               routes.MapRoute(
+                name: "underwear/briefs",
+                url: "WOMAN/underwear/briefs/page/{page}/{sort}",
+                defaults: new { controller = "Home", action = "categoryPage", catalogAId = 1, catalogBId = 10, page = UrlParameter.Optional , sort=UrlParameter.Optional }
+            );
+
                       
 
               routes.MapRoute(
