a74917438cbc6438281ac22780908ad7065b5c41
TakahashiKaede <j36ef37@gmail.com>
2022-03-04 16:37:57 +0800

    修改spu的bug

--- shop123/Scripts/sellerUpload.js
+++ shop123/Scripts/sellerUpload.js
@@ -3,6 +3,7 @@ Dropzone.options.dropzoneform = {
     addRemoveLinks: true,
     autoProcessQueue: false,
     maxFiles: 5,
+    parallelUploads: 5, //允許多張上傳數量，沒有設定的話，默認為2
     maxFilesize: 5,//MB
     dictDefaultMessage: '請點擊或將圖片拖曳至此區塊',
     init: function () {
